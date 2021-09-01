043a342c98914ad892cfd199fa440ef6c7f77bfa
soomin <82ryusm@naver.com>
2021-09-02 00:15:02 +0900

    0901 HomeActivity 가완성

--- app/src/main/java/com/example/nachos/HomeActivity.java
+++ app/src/main/java/com/example/nachos/HomeActivity.java
@@ -11,9 +11,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.viewpager.widget.PagerAdapter;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
 import java.util.ArrayList;
@@ -21,6 +24,8 @@ import java.util.ArrayList;
 public class HomeActivity extends AppCompatActivity {
     private ArrayList<Integer> imageList;
     private static final int DP = 24; //수치가 높을수록 옆에 그림이 조금씩 더 많이보임
+    private RecyclerView listview;
+    private MyAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,16 +42,74 @@ public class HomeActivity extends AppCompatActivity {
         viewPager.setPageMargin(margin/2);
 
         viewPager.setAdapter(new ViewPagerAdapter(this, imageList));
+
+        ArrayList<String> list = new ArrayList<>();
+
+        for (int i=0; i<100; i++) {
+            list.add(String.format("come on %d", i)) ;
+        }
+
+        /*
+        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
+        RecyclerView recyclerView = findViewById(R.id.recycler1) ;
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)) ;
+
+        // 리사이클러뷰에 TestAdapter01 객체 지정.
+        TestAdapter01 adapter = new TestAdapter01(list) ;
+        recyclerView.setAdapter(adapter) ;*/
+
+        init();
     }
+
+    private void init() {
+
+        listview = findViewById(R.id.recycler1);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        listview.setLayoutManager(layoutManager);
+
+        ArrayList<String> itemList = new ArrayList<>();
+        itemList.add("0");
+        itemList.add("1");
+        itemList.add("2");
+        itemList.add("3");
+        itemList.add("4");
+        itemList.add("5");
+        itemList.add("6");
+        itemList.add("7");
+        itemList.add("8");
+        itemList.add("9");
+        itemList.add("10");
+        itemList.add("11");
+
+        adapter = new MyAdapter(this, itemList, onClickItem);
+        listview.setAdapter(adapter);
+
+        MyListDecoration decoration = new MyListDecoration();
+        listview.addItemDecoration(decoration);
+    }
+
+    private View.OnClickListener onClickItem = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            String str = (String) v.getTag();
+            Toast.makeText(HomeActivity.this, str, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
 
     public void initializeData() //이미지를 저장함 배열에
     {
         imageList = new ArrayList();
 
-        imageList.add(R.drawable.aboutve);
-        imageList.add(R.drawable.aboutve);
-        imageList.add(R.drawable.aboutve);
-        imageList.add(R.drawable.aboutve);
-        imageList.add(R.drawable.aboutve);
+        imageList.add(R.drawable.home_meaning);
+        imageList.add(R.drawable.main_vegan);
+        imageList.add(R.drawable.home_meaning);
+        imageList.add(R.drawable.main_vegan);
+        imageList.add(R.drawable.home_meaning);
+
+
     }
+
+
 }
