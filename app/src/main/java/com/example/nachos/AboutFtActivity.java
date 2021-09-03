2b15e417b74d5b8caea8909fe9d5ffc0aba4a21d
jis00na <surisuri15@kookmin.ac.kr>
2021-09-03 21:58:34 +0900

    0903_1

---
+++ app/src/main/java/com/example/nachos/AboutFtActivity.java
@@ -0,0 +1,20 @@
+package com.example.nachos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+import android.os.Bundle;
+
+public class AboutFtActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_ft);
+    }
+}
